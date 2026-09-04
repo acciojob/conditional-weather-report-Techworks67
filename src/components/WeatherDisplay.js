7c56a631facff5b550321daff89b078634809a2c
Techworks67 <166513600+Techworks67@users.noreply.github.com>
2026-09-05 04:57:51 +0530

    Create WeatherDisplay.js

---
+++ src/components/WeatherDisplay.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const WeatherDisplay = ({ weather }) => {
+  const color = weather.temperature > 20 ? "red" : "blue";
+
+  return (
+    <div>
+      <p>Temperature: <span style={{ color: color }}>{weather.temperature}</span></p>
+      <p>Conditions: {weather.conditions}</p>
+    </div>
+  );
+};
+
+export default WeatherDisplay;
